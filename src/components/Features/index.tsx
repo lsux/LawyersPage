3d1addb1a24cb5e53945b3edb972fde680ac3c77
Alexei Hernandez <alexei2000.13@gmail.com>
2024-09-24 20:12:23 -0400

    delete yarn

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -12,7 +12,7 @@ const Features = () => {
             paragraph={MAIN_SECTORS_DESCRIPTION}
             center
           />
-          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-4">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-5">
             {SECTORS.map((sector) => (
               <SingleFeature key={sector.code} sector={sector} />
             ))}
